611d2fb71a3230fd6269915a757ecce485c91660
Berry-mas <junseo9145@gmail.com>
2025-07-01 10:31:43 +0900

    first commit

---
+++ src/test/java/com/smile/jenkins/Chap12JenkinsLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smile.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap12JenkinsLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
